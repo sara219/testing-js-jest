ec4ffa913a7b99909e32bc3ed88ce2d6ae3da163
sara219 <sara97abuawwad@gmail.com>
2022-06-10 19:53:43 +0300

    make it better

--- js/sum.test.js
+++ js/sum.test.js
@@ -3,5 +3,5 @@ const sum2num = require('./sum')
 // ! write test function => the first parameter is the name (string) of the test and the other is a function where we write the test
 test('adds two numbers', () => {
     
-    expect(sum2num(1, 2)).toBe(3)
+    expect(sum2num(2, 2)).toBe(4)
 })
